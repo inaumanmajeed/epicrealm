ac2bc5ffb7fc57e296ba560881c032d1d52ab17a
Nauman Majeed <inaumanmajeed@gmail.com>
2024-07-18 21:33:09 +0500

    chore: Update package.json to set "private" to true

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from "react-router-dom";
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import '../node_modules/elegant-icons/style.css';
@@ -19,9 +20,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
