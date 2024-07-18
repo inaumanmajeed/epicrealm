0bd71d09dcdd8701cd09361f90bf4e882cbe6dde
Nauman Majeed <inaumanmajeed@gmail.com>
2024-07-18 21:37:34 +0500

    chore: Remove unused HashRouter import and wrap App component with React.StrictMode

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from "react-router-dom";
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import '../node_modules/elegant-icons/style.css';
@@ -20,9 +19,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <React.StrictMode>
     <App />
-  </HashRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
